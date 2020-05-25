62a3b241bf739bd386000b9cafa9c3a253679bd1
Mrwindwaker <lagr45@gmail.com>
2020-05-25 11:16:11 -0500

    Se creo Rama Test

--- appMono/models.py
+++ appMono/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
 class TestClass(models.Model):
     user = models.CharField()
     description = models.TextField()
+    password = models.CharField()
